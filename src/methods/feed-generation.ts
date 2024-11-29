46e03fb161e45a8d1ebd63be93feaf4f66ade1b8
luna <luna@wvvw.me>
2024-11-30 00:11:36 +1030

    fix: log which algo the error happened with

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -5,28 +5,11 @@ import algos from '../algos';
 import { validateAuth } from '../auth';
 import { AtUri } from '@atproto/syntax';
 
-function retry<T>(fn: () => Promise<T> | T, maxAttempts = 3): Promise<T> {
-  return new Promise<T>(async (resolve, reject) => {
-    let lastError: Error;
-
-    for (let attempt = 0; attempt < maxAttempts; attempt++) {
-      try {
-        const result = await fn();
-        return resolve(result);
-      } catch (error) {
-        lastError = error as Error;
-        if (attempt === maxAttempts - 1) {
-          reject(lastError);
-        }
-      }
-    }
-  });
-}
-
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.getFeedSkeleton(async ({ params, req }) => {
+    const feedUri = new AtUri(params.feed);
+
     try {
-      const feedUri = new AtUri(params.feed);
       const algo = algos[feedUri.rkey].handler;
 
       if (feedUri.hostname !== ctx.cfg.publisherDid || feedUri.collection !== 'app.bsky.feed.generator' || !algo) {
@@ -35,9 +18,7 @@ export default function (server: Server, ctx: AppContext) {
 
       // Only check auth if the algorithm requires it
       const requiresAuth = algos[feedUri.rkey].requiresAuth;
-      const requesterDid = requiresAuth
-        ? await retry(() => validateAuth(req, ctx.cfg.serviceDid, ctx.didResolver))
-        : undefined;
+      const requesterDid = requiresAuth ? await validateAuth(req, ctx.cfg.serviceDid, ctx.didResolver) : undefined;
 
       console.info(`[${feedUri.rkey}] ${requesterDid ?? 'unknown'} ${JSON.stringify(params)}`);
 
@@ -47,7 +28,7 @@ export default function (server: Server, ctx: AppContext) {
         body: body,
       };
     } catch (error) {
-      console.error('Error in feed generation', JSON.stringify(error));
+      console.error(`[${feedUri.rkey}] Error in feed generation`, JSON.stringify(error));
       throw error;
     }
   });
