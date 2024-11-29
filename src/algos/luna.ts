f226210503aeebbdeb06dda1bfd7b968f9340303
luna <luna@wvvw.me>
2024-11-29 21:46:51 +1030

    fix: use did not handle

--- src/algos/luna.ts
+++ src/algos/luna.ts
@@ -7,7 +7,7 @@ export const shortname = 'luna';
 export const handler = async (ctx: AppContext, params: QueryParams) => {
   const feed = [
     {
-      post: 'at://imlunahey.com/app.bsky.feed.post/3lc364tfdhk2l',
+      post: 'at://did:plc:k6acu4chiwkixvdedcmdgmal/app.bsky.feed.post/3lc364tfdhk2l',
     },
   ];
 
