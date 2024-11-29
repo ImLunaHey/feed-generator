a8ad70e417b28ec8cabeb233079f9a26282d9bd1
luna <luna@wvvw.me>
2024-11-29 23:21:13 +1030

    fix: better error logging

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -7,23 +7,28 @@ import { AtUri } from '@atproto/syntax';
 
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.getFeedSkeleton(async ({ params, req }) => {
-    const feedUri = new AtUri(params.feed);
-    const algo = algos[feedUri.rkey].handler;
+    try {
+      const feedUri = new AtUri(params.feed);
+      const algo = algos[feedUri.rkey].handler;
 
-    if (feedUri.hostname !== ctx.cfg.publisherDid || feedUri.collection !== 'app.bsky.feed.generator' || !algo) {
-      throw new InvalidRequestError('Unsupported algorithm', 'UnsupportedAlgorithm');
+      if (feedUri.hostname !== ctx.cfg.publisherDid || feedUri.collection !== 'app.bsky.feed.generator' || !algo) {
+        throw new InvalidRequestError('Unsupported algorithm', 'UnsupportedAlgorithm');
+      }
+
+      // Only check auth if the algorithm requires it
+      const requiresAuth = algos[feedUri.rkey].requiresAuth;
+      const requesterDid = requiresAuth ? await validateAuth(req, ctx.cfg.serviceDid, ctx.didResolver) : undefined;
+
+      console.info(`[${feedUri.rkey}] ${requesterDid ?? 'unknown'} ${JSON.stringify(params)}`);
+
+      const body = await algo(ctx, params, requesterDid);
+      return {
+        encoding: 'application/json',
+        body: body,
+      };
+    } catch (error) {
+      console.error('Error in feed generation', error);
+      throw error;
     }
-
-    // Only check auth if the algorithm requires it
-    const requiresAuth = algos[feedUri.rkey].requiresAuth;
-    const requesterDid = requiresAuth ? await validateAuth(req, ctx.cfg.serviceDid, ctx.didResolver) : undefined;
-
-    console.info(`[${feedUri.rkey}] ${requesterDid ?? 'unknown'} ${JSON.stringify(params)}`);
-
-    const body = await algo(ctx, params, requesterDid);
-    return {
-      encoding: 'application/json',
-      body: body,
-    };
   });
 }
