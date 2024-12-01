0794085f43431e504eefd1b3ec4883d3fc93d716
luna <luna@wvvw.me>
2024-12-01 18:05:30 +1030

    fix: types

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import { Database } from './db';
+import { DidResolver } from '@atproto/identity';
 
 export const config = {
   hostname: process.env.FEEDGEN_HOSTNAME || 'example.com',
@@ -6,4 +8,17 @@ export const config = {
   port: process.env.FEEDGEN_PORT || 3000,
   sqliteLocation: process.env.FEEDGEN_SQLITE_LOCATION || ':memory:',
   publisherDid: process.env.FEEDGEN_PUBLISHER_DID || 'did:example:alice',
+};
+
+type Config = typeof config;
+
+export type AppContext = {
+  db: Database;
+  didResolver: DidResolver;
+  cfg: Config;
+};
+
+export type GeneratorContext = {
+  db: Database;
+  config: Config;
 };
